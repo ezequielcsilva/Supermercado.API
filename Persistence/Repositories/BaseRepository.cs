ba1f90e89ceb5038d7d6fea589921b2e171ad62b
ezequielcsilva <falecomezequielcardoso@gmail.com>
2021-08-06 09:11:53 -0300

    feat: add BaseRepository

---
+++ Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,12 @@
+using Supermercado.API.Persistence.Contexts;
+
+namespace Supermercado.API.Persistence.Repositories
+{
+    public class BaseRepository {
+      protected readonly AppDbContext _context;
+
+      public BaseRepository(AppDbContext context) {
+        _context = context;
+      }
+    }
+}
